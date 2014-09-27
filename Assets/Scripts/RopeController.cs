85d6605a071b63d1d84b08e2c5a6880600f65b55
Valentin Slawicek <valentin.slawi@gmail.com>
2014-09-27 21:30:00 +0200

    Refactor and update rope controller to smaller game ui

--- Assets/Scripts/RopeController.cs
+++ Assets/Scripts/RopeController.cs
@@ -7,21 +7,21 @@ public class RopeController : MonoBehaviour {
 		Transform climber = GameObject.Find("ClimberCharacter").transform;
 		float distance = (jumper.position - climber.position).magnitude;
 
-		float radius = distance * distance / transform.childCount * 0.1f;
-		radius = Mathf.Min(0.5f, Mathf.Max(0.1f, radius));
+		float radius = distance * distance / transform.childCount * 0.05f;
+		radius = Mathf.Min(0.25f, Mathf.Max(0.05f, radius));
 
 		Transform prevChild = null;
 		foreach (Transform child in transform) {
 			child.GetComponent<CircleCollider2D>().radius = radius;
 			if (prevChild != null) {
 				// break rope
-				if ((child.position - prevChild.position).magnitude > 2.6) {
+				if ((child.position - prevChild.position).magnitude > 1.3) {
 					child.GetComponent<HingeJoint2D>().enabled = false;
 					// TODO loose game
 
 					// reset chain
 					foreach (Transform innerChild in transform) {
-						innerChild.GetComponent<CircleCollider2D>().radius = 0.1f;
+						innerChild.GetComponent<CircleCollider2D>().radius = 0.05f;
 					}
 					return;
 				}
@@ -36,8 +36,8 @@ public class RopeController : MonoBehaviour {
 			climber.GetComponent<Rigidbody2D>().mass = 0.01f;
 			if (distance > 13) {
 				// push climber up (in case due to current physics this won't happen)
-				climber.rigidbody2D.AddForce(new Vector2(0, distance * 0.01f));
-				climber.Translate(new Vector2(0, distance * 0.01f));
+				climber.rigidbody2D.AddForce(new Vector2(0, distance * 0.005f));
+				climber.Translate(new Vector2(0, distance * 0.005f));
 			}
 		}
 	}
