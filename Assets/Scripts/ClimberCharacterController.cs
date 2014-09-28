73f666240e323f6bffaa57f1fede849049507540
Valentin Slawicek <valentin.slawi@gmail.com>
2014-09-28 10:07:19 +0200

    Prefabs for mainLevel, improve climber handling

--- Assets/Scripts/ClimberCharacterController.cs
+++ Assets/Scripts/ClimberCharacterController.cs
@@ -7,6 +7,7 @@ public class ClimberCharacterController : MonoBehaviour
 	[SerializeField] float maxSpeed = 10f;				// The fastest the player can travel in the x axis.	
 
 	
+	[SerializeField] bool airControl = false;			// Whether or not a player can steer while jumping;
 	[SerializeField] LayerMask whatIsGround;			// A mask determining what is ground to the character
     [SerializeField] LayerMask whatIsClimbable;			// A mask determining what is ground to the character
 	
@@ -33,38 +34,32 @@ public class ClimberCharacterController : MonoBehaviour
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
         walled = Physics2D.OverlapCircle(charCenter, charRadius, whatIsClimbable);
-
-
-
 	}
 
 
 	public void Move(float move, float climbing)
 	{
-		// Move the character
-			
-		// If the input is moving the player right and the player is facing left...
-		if(move > 0 && !facingRight)
-        {
-            Flip();// ... flip the player.
-		// Otherwise if the input is moving the player left and the player is facing right...
-        }else if(move < 0 && facingRight)
-        {
-            Flip();// ... flip the player.
-        }  
- 
-		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+		//only control the player if grounded or airControl is turned on
+		if (grounded || airControl) {
+			// If the input is moving the player right and the player is facing left...
+			if (move > 0 && !facingRight) {
+				Flip ();// ... flip the player.
+					// Otherwise if the input is moving the player left and the player is facing right...
+			} else if (move < 0 && facingRight) {
+				Flip ();// ... flip the player.
+			}  
 
-        if (move != 0 && grounded && !walled)
-        {
-            anim.SetInteger("state", 1);
-            //Debug.Log("1");
+			rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
 
-        }
+			if (move != 0 && grounded && !walled) {
+				anim.SetInteger ("state", 1);
+				//Debug.Log("1");
+			}
+		}
         
         if (walled)
         {
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, climbing * 13f);
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, climbing * 7f);
             anim.SetInteger("state", 2);
             //Debug.Log("2");
         }
