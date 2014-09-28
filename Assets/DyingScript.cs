9cd1f3b37ba53ea7c88c3896d5739fba7baa7b2d
Patrick Weiner <pawe@sbox.tugraz.at>
2014-09-28 14:40:21 +0200

    audio sources

--- Assets/DyingScript.cs
+++ Assets/DyingScript.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class DyingScript : MonoBehaviour {
 
 	// Update is called once per frame
+    public AudioClip dying;
+
 	void Update () {
         if (GameObject.Find("JumperCharacter").GetComponent<JumperCharacterController>().isAlreadyDead && GameObject.Find("ClimberCharacter").GetComponent<ClimberCharacterController>().isAlreadyDead)
         {
-            audio.Play("dying");
+            audio.PlayOneShot(dying);
         }
 	}
 }
