bd659a48da0fded5518c10e0cbd419cfbbfb0ee9
Valentin Slawicek <valentin.slawi@gmail.com>
2014-09-28 12:58:21 +0200

    Add Icicle and trigger

---
+++ Assets/Scripts/Icicle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Icicle : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if ((col.gameObject.layer == LayerMask.NameToLayer("Jumper"))
+		    || (col.gameObject.layer == LayerMask.NameToLayer("Climber"))) {
+			Debug.Log("Die"); // TODO die
+		} else if (col.gameObject.layer == LayerMask.NameToLayer("Rope")) {
+			col.transform.GetComponent<HingeJoint2D>().enabled = false;
+			// TODO die
+		}
+	}
+}
