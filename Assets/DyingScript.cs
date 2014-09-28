e6b77f945999ee8d7c12937f9b8a4d106db33b72
Patrick Weiner <pawe@sbox.tugraz.at>
2014-09-28 14:26:29 +0200

    Sounds added to characters and dying sound in progress

---
+++ Assets/DyingScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DyingScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.Find("JumperCharacter").GetComponent<JumperCharacterController>().isAlreadyDead && GameObject.Find("ClimberCharacter").GetComponent<ClimberCharacterController>().isAlreadyDead)
+        {
+            audio.Play("dying");
+        }
+	}
+}
